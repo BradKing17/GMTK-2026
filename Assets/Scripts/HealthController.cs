3d3e4c6a1f17946be27df507f83a6c2b54d53c89
Matthew Bowes <matt.j.bowes@gmail.com>
2026-07-22 22:15:12 +0100

    added heath shader and controller
    health shader controller can be called to either immediately set health or smoothly lerp to new destination level, and set the text as well

---
+++ Assets/Scripts/HealthController.cs
@@ -0,0 +1,34 @@
+using Godot;
+using System;
+
+public partial class HealthController : Node
+{
+    [Export(PropertyHint.Range, "0,1")] public float fillValue = 0;
+    private Material material;
+    private RichTextLabel textLabel;
+    private float time;
+    public override void _Ready()
+    {
+        material = GetChild<Sprite2D>(0).Material;
+        (material as ShaderMaterial).SetShaderParameter("fill_value", fillValue);
+        SlerpToFillLevel(fillValue, 1, 10f);
+    }
+
+    public void SetFillLevel(float fV)
+    {
+        (material as ShaderMaterial).SetShaderParameter("fill_value", fV);
+    }
+
+    public void SlerpToFillLevel(float fromLevel, float destinationLevel, float t)
+    {
+        Tween tween = GetTree().CreateTween();
+        tween.SetParallel(true);
+        tween.TweenMethod(Callable.From<int>(SetLabelText), fromLevel * 100, destinationLevel * 100, t);
+        tween.TweenProperty(material,"shader_parameter/fill_value", destinationLevel, t);
+        tween.Chain();
+    }
+    private void SetLabelText(int value)
+    {
+        GetChild<RichTextLabel>(1).Text = $"{value} ml";
+    }
+}
