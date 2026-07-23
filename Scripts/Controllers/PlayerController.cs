6003b38ccdbae11424f4aeb5a8b9a9343f5bfd98
Bradley King <king.brad1997@gmail.com>
2026-07-23 17:55:18 +0100

    Attack added to player

--- Scripts/Controllers/PlayerController.cs
+++ Scripts/Controllers/PlayerController.cs
@@ -8,7 +8,14 @@ public partial class PlayerController : CharacterBody2D
 		[Export] private float health = 5500.0f;
 		[Export] private float rateOfBloodLoss = 10.0f;
 		[Export] private int holes = 0;
-		private bool canMove = false;
+
+		[Export] private float MaxRadius = 500.0f;
+
+		[Export] private AnimatedSprite2D playerSprite;
+
+		private Vector2 target;
+		private bool canMove = true;
+		private bool isAttacking = false;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -16,12 +23,42 @@ public partial class PlayerController : CharacterBody2D
 		GameStart();
 	}
 
+	public void GameStart()
+	{
+		Print("Game Started");
+		holes = 1;
+		canMove = true;
+	}
+
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(double delta)
 	{
-		GetInput();
-		MoveAndSlide();
-
+		//Attacking and moving logic
+		if(isAttacking)
+		{
+			Velocity = Position.DirectionTo(target) * moveSpeed * 2;
+			if (Position.DistanceTo(target) > 20.0f)
+        	{
+        		MoveAndSlide();	
+        	}
+			else
+			{	
+				Velocity = Vector2.Zero;
+				isAttacking = false;
+				playerSprite.Frame = 0;
+			}
+		}
+		else
+		{
+			GetInput();
+			MoveAndSlide();
+		}
+		
+	
+	
+		
+		
+		//Health drain logic
 		if(holes > 0)
 		{
 			health -= (float)delta * holes * rateOfBloodLoss;
@@ -35,16 +72,31 @@ public partial class PlayerController : CharacterBody2D
 
 		Print(health);
 	}
-	public void GetInput()
+
+ public override void _Input(InputEvent @event)
     {
-        Vector2 inputDirection = Input.GetVector("move_left", "move_right", "move_up", "move_down");
-        Velocity = inputDirection * moveSpeed;
+		
+        if (@event.IsActionPressed("attack"))
+        {
+			isAttacking = true;
+			Vector2 clickPosition = GetGlobalMousePosition();
+			 Vector2 offset = clickPosition - GlobalPosition;
+            float distance = offset.Length();
+
+            target = GlobalPosition + (offset.Normalized() * MaxRadius);
+
+			playerSprite.Frame = 1;
+        }
     }
 
-	public void GameStart()
-	{
-		Print("Game Started");
-		holes = 1;
-		canMove = true;
-	}
+	public void GetInput()
+    {
+		if(!isAttacking)
+		{
+        	Vector2 inputDirection = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+        	Velocity = inputDirection * moveSpeed;
+		}
+    }
+
+
 }
